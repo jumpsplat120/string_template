fb0007cf013d9278be2f143d4db5603c381a759b
jumpsplat120 <jumpsplat120@yahoo.com>
2024-05-27 01:28:58 -0700

    Add recursion system, and env system
    Upvalues do not exist in the function by default, and rather than iterate through all upvalues for potentially only one access every time TL is called, we make it so you can simply pass in a custom env table. the env table will add a mt to it that has a __index pointing to _G so we can still access global values if local ones are added.
    
    We also add a hidden third param called recursive, that is only set to true internally. This is so that when calling something like TL"%{'Hello '.. %{'wor' .. 'ld'}}", rather than world getting returned as just `world`, and causing the second concat to fail because there's no `world` variable, we instead replace it with an id pointing towards the actual value that was returned, and place that in env. Only if recursive is false do we return the true string value as expected.

--- init.lua
+++ init.lua
@@ -1,13 +1,31 @@
-function TL(str, env)
+local mt = { __index = _G }
+
+function TL(str, env, recursive)
+    env = env or {}
+    
+    setmetatable(env, mt)
+
     local result = str:gsub("(%%?)%%(%b{})", function(prev, expression)
         if prev == "%" then
-            return prev .. TL(expression)
+            return prev .. TL(expression, env)
         end
 
         expression = expression:sub(2, -2)
-        expression = assert(loadstring("return " .. TL(expression)))
+        expression = assert(loadstring("return " .. TL(expression, env, true)))
         
         setfenv(expression, env)
+        
+        if recursive then
+            local id = "_"
+
+            for i = 1, 5, 1 do
+                id = id .. string.char(math.random() > 0.5 and math.random(65, 90) or math.random(97, 122))
+            end
+
+            env[id] = expression()
+
+            return id
+        end
 
         return tostring(expression())
     end)
